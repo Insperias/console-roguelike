ab5660348bb87cb5ce09ab323d129a285213541e
Insperias <insperias.inm@gmail.com>
2019-11-22 01:56:08 +0300

    Starting roguelike with c++ and libtcod. Generating random map.

---
+++ lintcod_game/lintcod_game.cpp
@@ -0,0 +1,37 @@
+﻿// lintcod_game.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include "C:\DEV\libtcod-1.15.0-x86_64-msvc\include\libtcod.hpp"
+
+int main() {
+	int playerx = 40, playery = 25;
+	TCODConsole::initRoot(80, 50, "libtcod C++ tutorial", false);
+	while (!TCODConsole::isWindowClosed()) {
+		TCOD_key_t key;
+		TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS, &key, NULL);
+		switch (key.vk) {
+		case TCODK_UP: playery--; break;
+		case TCODK_DOWN: playery++; break;
+		case TCODK_LEFT: playerx--; break;
+		case TCODK_RIGHT: playerx++; break;
+		default:break;
+		}
+		TCODConsole::root->clear();
+		TCODConsole::root->putChar(playerx, playery, '@');
+		TCODConsole::flush();
+	}
+	return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
